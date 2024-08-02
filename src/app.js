03ef63e62cbf3512339081b72e6a70a56d4fc57f
Ricard Mesegué <ricardm1995@gmail.com>
2024-08-02 19:11:32 +0000

    feat: Generador de Dominios

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  let domainList = document.getElementById("domain-list");
+
+  let html = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        let domain = `${pronoun[i]}${adj[j]}${noun[k]}.com`;
+        html += `<li class="list-group-item">${domain}</li>`;
+      }
+    }
+  }
+  domainList.innerHTML = html;
 };
